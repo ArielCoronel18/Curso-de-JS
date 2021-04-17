63e5046f8c0f31dcecc52155558c4cba4b4b767e
Ariel Coronel <milanari@hotmail.com>
2021-04-17 00:03:53 -0500

    Inicio de curso de JS

---
+++ 2-Variables.js
@@ -0,0 +1,11 @@
+var variable1= 2;
+var variable2= 3;
+
+var suma= variable1 + variable2;
+var resta= variable1 + variable2;
+var multiplicacion= variable1 * variable2;
+var division= variable1 / variable2;
+
+var variable3= 1;
+variable3 += 1;
+alert(variable3);
---
+++ 3-Cadenas.js
@@ -0,0 +1,17 @@
+var texto = 'Hola mundo';
+
+var miTexto = 'Hola, mi nombre es ';
+miTexto = miTexto + 'Ariel';
+
+var edad = 'mi edad es: ';
+edad = edad + 32;
+
+var textoNumerico = '20' + 10;
+
+var casas = 10;
+var tiendas = 20;
+var parques = 5;
+
+var reporte= 'Tenemos ' + casas + ' casas, tenemos ' + tiendas + ' tiendas y tenemos ' + parques + ' parques';
+var reporte2= `Tenemos ${casas} casas, tenemos ${tiendas} tiendas y tenemos ${parques} parques`
+alert(reporte2);
