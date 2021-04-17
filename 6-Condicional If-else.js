0c91cf09ceea9936f03a45612845b02baccda498
Ariel Coronel <milanari@hotmail.com>
2021-04-17 13:19:04 -0500

    Added Condicional If else

---
+++ 6-Condicional If-else.js
@@ -0,0 +1,10 @@
+var miVariable = 10;
+var condicional = miVariable != 10;
+var condicional2 = miVariable > 5;
+
+
+if(condicional && condicional2){
+    alert('Se cumple la condición')
+}else{
+    alert('No se cumple la condición')
+}
---
+++ 6-Condicional-If.js
@@ -0,0 +1,11 @@
+var miVariable = 20;
+var condicional = miVariable != 10
+
+// <, >, <=, >=, !=, ==, ! // 
+var condicional2 = miVariable > 5;
+
+// &&, || //
+var edad = 29;
+if(edad){
+    alert('Se cumple la condición')
+}
